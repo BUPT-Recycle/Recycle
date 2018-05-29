97b8230ba9ec9e829114ef8b6b5ee50ffca1dc2d
谭上鸥 <tanshangou@hotmail.com>
2018-05-29 19:50:43 +0800

    订单接口v2

--- src/main/java/com/bupt/recycle/service/impl/WorkerServiceImpl.java
+++ src/main/java/com/bupt/recycle/service/impl/WorkerServiceImpl.java
@@ -6,7 +6,6 @@ import com.bupt.recycle.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.jvm.hotspot.jdi.DoubleValueImpl;
 
 /**
  * @anthor tanshangou
